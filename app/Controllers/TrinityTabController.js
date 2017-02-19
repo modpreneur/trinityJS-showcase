20da4fa298533d90279eb7ddeb3574a249b5432e
Zbynek Fiser <fiser@modpreneur.com>
2017-02-19 21:37:17 +0100

    ## TrinityTab2 callback lifecycle functions

--- app/Controllers/TrinityTabController.js
+++ app/Controllers/TrinityTabController.js
@@ -1,11 +1,29 @@
 'use strict';
 
+import $ from 'jquery';
 import Controller from 'trinity/Controller';
 import TrinityTab from 'trinity/components/TrinityTab2';
 
 export default class TrinityTabController extends Controller {
     indexAction($scope){
-        let tab = new TrinityTab();
+        const SHOW_TAB = 'tab1',
+            EDIT_TAB = 'tab2',
+            THIRD_TAB = 'tab3';
+
+
+        let tab = new TrinityTab($('.tab-head'), {
+            [SHOW_TAB] : {
+                onLoad: (...params) => {
+                    console.log('onLoad function called', params)
+                },
+                onActive: (...params) => {
+                    console.log('onActive called', params);
+                },
+                onDelete: (...params) => {
+                    console.log('onDelete called', params);
+                }
+            }
+        });
         console.log(tab);
     }
 }
